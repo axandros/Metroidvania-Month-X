aa53ff9a470081d79388bb1026a8f67a5fed4530
Brumagint <brumagint@outlook.com>
2020-11-22 15:50:46 -0500

    Added additional tiles.

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlatformerCharacter))]
+[RequireComponent(typeof(PlayerHealth))]
+[RequireComponent(typeof(PlayerShooting))]
+public class PlayerManager : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
